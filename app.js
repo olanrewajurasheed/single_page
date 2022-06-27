dcf71c6eec0a665b6182b68e9a0a19ccfa26e7bf
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-06-27 22:47:52 +0100

    form validation

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // Variables
 const claimBtn = document.querySelector("button"),
   firstName = document.querySelector("#firstname"),
-  lastName = document.querySelector("#lastName"),
+  lastName = document.querySelector("#lastname"),
   email = document.querySelector("#email"),
   password = document.querySelector("#password"),
-  errorText = document.querySelector("text");
+  errorText = document.querySelector("text"),
+  first = document.querySelector(".first");
 
 // Event Listeners
 eventListeners();
@@ -16,27 +17,63 @@ function eventListeners() {
 // Functions
 
 function claimBtnFnc() {
+  //validate first name
   validateFirstName();
-  
+
+  // validate last name input field
   validateLastName();
+
+  // validate email input field
+  validateEmail();
+
+  //validate password field
+  validatePassword();
+
 }
 
+// Excess DRY here, what do you expect from a noob?
+
 function validateFirstName() {
-  if (firstName.value.length == 0) {
+  if (firstName.value.length !== 0) {
+    firstName.style.borderColor = "lightblue";
+    firstName.classList.remove("img_icon");
+  } else {
     firstName.style.borderColor = "red";
     firstName.classList.add("img_icon");
-    document.getElementById("error_textfirst").classList.add("error_textfirst");
-  } else {
-    firstName.style.borderColor = "lightblue";
+
+    /* const firstText = document.createElement("text");
+    firstText.classList = "error_textfirst";
+    firstText.textContent = "First Name cannot be empty";
+    first.appendChild(firstText); */
   }
 }
 
-function validateLastName () {
-    if (lastName.value.length == 0) {
-        lastName.style.borderColor = "red";
-        lastName.classList.add("img_icon");
-        document.getElementById("error_textlast").classList.add("error_textlast");
-      } else {
-        lastName.style.borderColor = "lightblue";
-      }
+function validateLastName() {
+  if (lastName.value.length !== 0) {
+    lastName.style.borderColor = "lightblue";
+    lastName.classList.remove("img_icon");
+  } else {
+    lastName.style.borderColor = "red";
+    lastName.classList.add("img_icon");
+  }
+}
+
+function validateEmail() {
+  if (email.value.length !== 0) {
+    email.style.borderColor = "lightblue";
+    email.classList.remove("img_icon");
+  } else {
+    email.style.borderColor = "red";
+    email.classList.add("img_icon");
+  }
+}
+
+function validatePassword() {
+  if (password.value.length !== 0) {
+    password.style.borderColor = "lightblue";
+    password.classList.remove("img_icon");
+  } else {
+    password.style.borderColor = "red";
+    password.classList.add("img_icon");
+  }
 }
