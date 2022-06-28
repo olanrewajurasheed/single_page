12489d222a0085638f4b961397b18cdba0b25063
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-06-28 01:17:00 +0100

    modified

--- app.js
+++ app.js
@@ -31,52 +31,57 @@ function claimBtnFnc() {
 
 }
 
-// Excess DRY here, what do you expect from a noob?
+// Excess DRY here, but what do you expect from a noob?
 
 function validateFirstName() {
   if (firstName.value.length !== 0) {
-    firstName.style.borderColor = "lightblue";
+    firstName.style.borderColor = "hsl(154, 59%, 51%)";
     firstName.classList.remove("img_icon");
     document.querySelector(".first text").style.display="none";
   } else {
-    firstName.style.borderColor = "red";
+    firstName.style.borderColor = "hsl(0, 100%, 74%)";
     firstName.classList.add("img_icon");
     document.querySelector(".first text").style.display="block";
+    document.querySelector(".first input").placeholder="";
   }
 }
 
 function validateLastName() {
   if (lastName.value.length !== 0) {
-    lastName.style.borderColor = "lightblue";
+    lastName.style.borderColor = "hsl(154, 59%, 51%)";
     lastName.classList.remove("img_icon");
     document.querySelector(".last text").style.display="none";
   } else {
-    lastName.style.borderColor = "red";
+    lastName.style.borderColor = "hsl(0, 100%, 74%)";
     lastName.classList.add("img_icon");
     document.querySelector(".last text").style.display="block";
+    document.querySelector(".last input").placeholder="";
   }
 }
 
 function validateEmail() {
   if (email.value.length !== 0) {
-    email.style.borderColor = "lightblue";
+    email.style.borderColor = "hsl(154, 59%, 51%)";
     email.classList.remove("img_icon");
     document.querySelector(".email text").style.display="none";
   } else {
-    email.style.borderColor = "red";
+    email.style.borderColor = "hsl(0, 100%, 74%)";
     email.classList.add("img_icon");
     document.querySelector(".email text").style.display="block";
+    document.querySelector(".email input").placeholder="email@example/com";
+    document.querySelector(".email input").placeholder.color="red";
   }
 }
 
 function validatePassword() {
   if (password.value.length !== 0) {
-    password.style.borderColor = "lightblue";
+    password.style.borderColor = "hsl(154, 59%, 51%)";
     password.classList.remove("img_icon");
     document.querySelector(".pass text").style.display="none";
   } else {
-    password.style.borderColor = "red";
+    password.style.borderColor = "hsl(0, 100%, 74%)";
     password.classList.add("img_icon");
     document.querySelector(".pass text").style.display="block";
+    document.querySelector(".pass input").placeholder="";
   }
 }
